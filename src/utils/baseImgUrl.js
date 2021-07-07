ace3cbd2c22d383f40421d78e5e9bee746cbe6c0
gongping1990 <gp646890523@sina.com>
2021-07-07 09:50:07 +0800

    接口调整5

--- src/utils/baseImgUrl.js
+++ src/utils/baseImgUrl.js
@@ -1,4 +1,4 @@
 export let baseImgUrl =
-    process.env.NODE_ENV == "development" ? "http://115.159.149.184:9009/assert/" : "http://115.159.149.184:9009/assert/"
+    process.env.NODE_ENV == "development" ? "http://115.159.149.184:9009/asset/" : "http://115.159.149.184:9009/asset/"
 
 
